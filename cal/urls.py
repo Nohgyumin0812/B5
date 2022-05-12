fede2d955a2c0055c41e2159225127809f9607dd
최고운 <cku7808@naver.com>
2022-05-12 16:46:58 +0900

    url.py - login 오류 수정

--- cal/urls.py
+++ cal/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
     path('event/new/', views.event, name='event_new'),
 	path('event/edit/int:event_id/', views.event, name='event_edit'),
-    path('login', auth_views.LoginView.as_view(template_name ='cal/login.html'), name ='login'), #  임시로 작성
+    path('login/', auth_views.LoginView.as_view(template_name ='cal/login.html'), name ='login'), #  임시로 작성
     path('signup/', views.signup, name ='signup'),
     path('group_making/', views.group_making, name='group_making'),
     path('group_managing/', views.group_managing, name='group_managing'),
