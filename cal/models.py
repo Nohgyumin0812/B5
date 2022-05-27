ca8a6444014a59ef6fce2d2e20f8e031cddd7f0c
Nohgyumin0812 <69832684+Nohgyumin0812@users.noreply.github.com>
2022-05-27 11:23:00 +0900

    스포츠출력수정

--- cal/models.py
+++ cal/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from common.models import CustomUser
+from django.utils.text import slugify
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -26,3 +28,32 @@ class CustomGroup(models.Model):
     #date = models.DateTimeField(null = True, blank = True, default='')
     sports = models.CharField(max_length=50, default='')
     friendname = models.CharField(max_length=50, default='')
+
+
+class CustomGroup(models.Model):
+    groupname = models.CharField('groupname', max_length=200, blank=False, null=False)
+    slug = models.SlugField(max_length=30, unique=True, allow_unicode=True, db_index=True, default="")
+
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='owner', null = True, blank = True)
+    members = models.ManyToManyField(CustomUser, verbose_name='members', related_name='members', blank = True, default='')
+    #date = models.DateTimeField(null = True, blank = True, default='')
+    sports = models.CharField(max_length=50, default='')
+    friendname = models.CharField(max_length=50, default='')
+
+    # title 새로 저장 시, slug 에 해당 title slugify하여 저장
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.groupname, allow_unicode=True)
+        super(CustomGroup, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.groupname
+
+class InviteStatus(models.Model):
+    groupname = models.ForeignKey(CustomGroup, on_delete = models.CASCADE, related_name = 'invites')
+    invited = models.ManyToManyField(CustomUser, related_name = 'invited', blank = True)
+    accepted = models.ManyToManyField(CustomUser, related_name='accepted', blank=True)
+    rejected = models.ManyToManyField(CustomUser, related_name='rejected', blank=True)
+
+    def __str__(self):
+        return self.groupname
--- cal/views.py
+++ cal/views.py
@@ -26,41 +26,42 @@ def calendar(request):
     day_datas = target.find_all('div', {'class': 'day_data'})
     weather_dic = {}
     sports_dic = {}
-    indoor_sports = [1, 2, 5, 6]
-    outdoor_sports =[3, 4]
-    for day_data in day_datas:
-        date_data = day_data.find('span', {'class': 'date'})
-        weather_inner = day_data.find_all('span', {'class': 'weather_inner'})
-        for weathers in weather_inner:
-            timeslot = weathers.find('span', {'class': 'timeslot'})
-            weather = weathers.find('i', {'class': 'ico'})
-            if timeslot.text == '오전':
-                # weather_dic[date_data.text] = []
-                # weather_dic[date_data.text].append(weather.text)
-                weather_dic[date_data.text] = weather.text
+    indoor_sports = ['3', '4', '6']
+    outdoor_sports = ['1', '2', '5']
+    try:
+        ## 그룹 종목 출력 ##
+        sportsall = CustomGroup.objects.get(owner_id=request.user.id).sports
+        sportsall = ast.literal_eval(sportsall)
+        sports = json.dumps(sportsall, ensure_ascii=False)
 
-                if weather.text == '흐리고 비'or weather.text == '비 또는 눈' or weather.text ==  '눈 또는 비' or weather.text == '가끔 비 또는 눈' \
-                        or weather.text == '한때 비 또는 눈' or weather.text == '가끔 눈 또는 비' or weather.text == '한때 눈 또는 비' or \
-                        weather.text == '안개' or weather.text == '연무' or weather.text == '박무 (엷은 안개)' or weather.text == '빗방울' \
-                        or weather.text == '눈날림' or weather.text == '낙뢰' or weather.text == '황사' or weather.text == '비' or weather.text == '눈':
-                    sports_dic[date_data.text] = indoor_sports
-                elif weather.text == weather.text == '맑음' or weather.text == '구름많음' or weather.text == '구름조금' or weather.text == '흐림':
-                    sports_dic[date_data.text] = outdoor_sports
-    print(weather_dic)
-    print(sports_dic)
+        ## 날씨 크롤링 ##
+        for day_data in day_datas:
+            date_data = day_data.find('span', {'class': 'date'})
+            weather_inner = day_data.find_all('span', {'class': 'weather_inner'})
+            for weathers in weather_inner:
+                timeslot = weathers.find('span', {'class': 'timeslot'})
+                weather = weathers.find('i', {'class': 'ico'})
+                if timeslot.text == '오전':
+                    #weather_dic[date_data.text] = []
+                    # weather_dic[date_data.text].append(weather.text)
+                    weather_dic[date_data.text] = weather.text
 
+                    if weather.text == '흐리고 비'or weather.text == '비 또는 눈' or weather.text ==  '눈 또는 비' or weather.text == '가끔 비 또는 눈' \
+                            or weather.text == '한때 비 또는 눈' or weather.text == '가끔 눈 또는 비' or weather.text == '한때 눈 또는 비' or \
+                            weather.text == '안개' or weather.text == '연무' or weather.text == '박무 (엷은 안개)' or weather.text == '빗방울' \
+                            or weather.text == '눈날림' or weather.text == '낙뢰' or weather.text == '황사' or weather.text == '비' or weather.text == '눈':
+                        sports_dic[date_data.text] = list(set(sportsall) & set(indoor_sports))
+                    elif weather.text == weather.text == '맑음' or weather.text == '구름많음' or weather.text == '구름조금' or weather.text == '흐림':
+                        sports_dic[date_data.text] = list(set(sportsall) & set(outdoor_sports))
+        print(weather_dic)
+        print(sports_dic)
 
     # file_path = "./sample.json"
     # with open(file_path, 'w') as outfile:
     #     json.dump(weather_dic, outfile, ensure_ascii=False)
-    data = json.dumps(weather_dic, ensure_ascii=False)
-    sports_date = json.dumps(sports_dic, ensure_ascii=False)
+        data = json.dumps(weather_dic, ensure_ascii=False)
+        sports_date = json.dumps(sports_dic, ensure_ascii=False)
 
-    ## 그룹 종목 출력 ##
-    try:
-        sportsall = CustomGroup.objects.get(owner_id = request.user.id).sports
-        sportsall = ast.literal_eval(sportsall)
-        sports = json.dumps(sportsall, ensure_ascii=False)
 
     ## 그룹 참여자 출력 ##
         owner = CustomUser.objects.get(id = request.user.id).username
@@ -110,4 +111,4 @@ def my_schedule(request):
 
     print(members)
 
-    return render(request, 'cal/my_schedule.html', {'data': data, 'sportsall':sports, 'membersall':members})
+    return render(request, 'cal/my_schedule.html', {'data': data, 'sportsall':sports, 'membersall':members, 'sports_date':sports_date})
