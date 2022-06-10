72660cae35f88033922a096711481c4ac95ecfdd
Nohgyumin0812 <69832684+Nohgyumin0812@users.noreply.github.com>
2022-06-11 04:45:09 +0900

    Update views.py

--- cal/views.py
+++ cal/views.py
@@ -330,13 +330,22 @@ def group_managing(request):
 
     ##그룹 요청##
 
+
     courses = InviteGroup.objects.filter(invite_user=request.user.username, invite_status =1).values()
-    invite_member = []
-    for i in range(len(courses)):
-        invite_member.append(courses[i]['group'])
+    courses = pd.DataFrame(courses)
+
+    courses['num_member'] = 0
+    courses['sports'] = "22"
+    for i in range(courses.shape[0]):
+        courses['num_member'][i] = len(ast.literal_eval(str(CustomGroup.objects.filter
+                                                            (groupname =  courses['group'][i]).values()[0]['friendname']).replace(' ','')))+ 1
+        courses['sports'] = CustomGroup.objects.filter(groupname = courses['group'][i]).values()[0]['sports']
+    courses = courses[['group', 'num_member','sports']].set_index('group').T.to_dict()
+
     invite_member_dic = {}
-    invite_member_dic['개인초대'] = invite_member
+    invite_member_dic['개인초대'] = courses
     print(invite_member_dic)
+
     invite_group = json.dumps(invite_member_dic, ensure_ascii=False)
 
     ##그룹 관리##
